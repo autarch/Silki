18c363f8a1df4006b7f9191f6ff84516e9fb58f0
Dave Rolsky <autarch@urth.org>
2009-08-04 14:59:25 -0500

    Get config from Config module

--- lib/Silki/Schema.pm
+++ lib/Silki/Schema.pm
@@ -7,13 +7,21 @@ use DBI;
 use Fey::ORM::Schema;
 use Fey::DBIManager::Source;
 use Fey::Loader;
-
+use Silki::Config;
 
 my $Schema;
 
+my $source;
+if ($Silki::Schema::TestSchema)
 {
+    has_schema $Silki::Schema::TestSchema
+}
+else
+{
+    my $dbi_config = Silki::Config->new()->dbi_config();
+
     my $source =
-        Fey::DBIManager::Source->new( dsn          => 'dbi:Pg:dbname=Silki',
+        Fey::DBIManager::Source->new( %{ $dbi_config },
                                       post_connect => \&_set_dbh_attributes,
                                     );
 
@@ -33,6 +41,21 @@ sub _set_dbh_attributes
     $dbh->do( 'SET TIME ZONE UTC' );
 
     return;
+}
+
+sub LoadAllClasses
+{
+    my $class = shift;
+
+    for my $table ( $class->Schema()->tables() )
+    {
+        my $class = 'Silki::Schema::' . $table->name();
+
+        ( my $path = $class ) =~ s{::}{/}g;
+
+        eval "use $class";
+        die $@ if $@ && $@ !~ /\Qcan't locate $path/i;
+    }
 }
 
 no Fey::ORM::Schema;
