383b2dc57f2040d516e02584c81bbeb580f5d363
Dave Rolsky <autarch@urth.org>
2009-09-26 22:43:48 -0500

    add ckeditor

---
+++ share/ckeditor/config.js
@@ -0,0 +1,15 @@
+CKEDITOR.editorConfig = function( config )
+{
+    config.toolbar_Silki =
+        [ [ "Bold", "Italic" ],
+          [ "H2", "H3", "H4", "-", "Pre" ],
+          [ "NumberedList", "BulletedList", "-", "Outdent", "Indent", "Blockquote" ],
+          [ "Link", "Unlink", "Anchor" ],
+          [ "Image", "Table", "HorizontalRule", "SpecialChar" ],
+          [ "Undo", "Redo", "Maximize" ] ];
+
+    config.toolbar = "Silki";
+
+    config.skin = "Silki";
+};
+
