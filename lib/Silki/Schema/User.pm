aa275dda21df06b713c884305ad0748918c0fe30
Dave Rolsky <autarch@urth.org>
2009-11-28 11:53:40 -0600

    Turn off aliases for terms

--- lib/Silki/Schema/User.pm
+++ lib/Silki/Schema/User.pm
@@ -719,12 +719,14 @@ sub _BuildAllWikiSelect {
     my $explicit_wiki_select = Silki::Schema->SQLFactoryClass()->new_select();
 
     my $is_explicit1 = Fey::Literal::Term->new('1 AS is_explicit');
+    $is_explicit1->set_can_have_alias(0);
     $explicit_wiki_select->select( $Schema->table('Wiki'), $is_explicit1 );
     $class->_ExplicitWikiSelectBase($explicit_wiki_select);
 
     my $implicit_wiki_select = Silki::Schema->SQLFactoryClass()->new_select();
 
     my $is_explicit0 = Fey::Literal::Term->new('0 AS is_explicit');
+    $is_explicit0->set_can_have_alias(0);
     $implicit_wiki_select->select( $Schema->table('Wiki'), $is_explicit0 );
     $class->_ImplicitWikiSelectBase($implicit_wiki_select);
 
