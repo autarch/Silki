f92790239d19f932046e91c3307a1ea028000a0f
Dave Rolsky <autarch@urth.org>
2010-03-04 11:23:36 -0600

    update Build.PL and Silki::Build to start being useful

--- inc/Silki/Build.pm
+++ inc/Silki/Build.pm
@@ -1,4 +1,39 @@
+package Silki::Build;
+
+use strict;
+use warnings;
+
+use base 'Module::Build';
 
 my %Requires = (
     'Catalyst::Plugin::Session::Store::DBI' => '0.15',
+    'Fey::ORM'                              => '0.32',
+    'MooseX::ClassAttribute'                => '0',
+    'MooseX::StrictConstructor'             => '0',
 );
+
+my %BuildRequires = (
+    'Test::Exception' => '0',
+    'Test::More'      => '0.88',
+);
+
+
+sub new {
+    my $class = shift;
+
+    return $class->SUPER::new(
+        license              => 'perl',
+        module_name          => 'Silki',
+        requires             => \%Requires,
+        build_requires       => \%BuildRequires,
+        script_files         => [ glob('bin/*.pl') ],
+        recursive_test_files => 1,
+        meta_merge           => {
+            resources => {
+                repository => 'http://hg.urth.org/hg/Silki',
+            },
+        },
+    );
+}
+
+1;
