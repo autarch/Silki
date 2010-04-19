b8a19b71f15f6b43c1e102f34983e1977c396fa5
Dave Rolsky <autarch@urth.org>
2010-04-19 13:56:33 -0500

    got old toolbar working again, along with list insertion

--- share/js-source/Textarea/Text.js
+++ share/js-source/Textarea/Text.js
@@ -60,4 +60,65 @@ else {
         this.textarea.selectionEnd = start + text.length;
         this.textarea.scrollTop = scroll;
     };
+
+    Textarea.Text.prototype.caretPosition = function () {
+        return this.textarea.selectionStart;
+    };
 }
+
+if ( document.createElement("textarea").setSelectionRange ) {
+    Textarea.Text.prototype.moveCaret = function (offset) {
+        var new_pos = this.caretPosition() + offset;
+
+        this.textarea.setSelectionRange( new_pos, new_pos );
+    };
+}
+else {
+    Textarea.Text.prototype.moveCaret = function (offset) {
+        var range = this.textarea.createTextRange();
+        range.collapse(true);
+        range.moveEnd( "character", this.caretPosition() + offset );
+        range.moveStart( "character", this.caretPosition() + offset );
+        range.select();
+    };
+}
+
+Textarea.Text.prototype.previousLine = function () {
+    var text = this.textarea.value;
+
+    var last_line_end = text.lastIndexOf( "\n", this.caretPosition() );
+
+    if ( ! last_line_end ) {
+        return "";
+    }
+    else {
+        var prev_line_start = text.lastIndexOf( "\n", last_line_end - 1 ) + 1;
+        return text.substr( prev_line_start, last_line_end - prev_line_start );
+    }
+}
+
+Textarea.Text.prototype.caretIsMidLine = function () {
+    var pos = this.caretPosition();
+
+    if ( pos == 0 ) {
+        return 0;
+    }
+
+    var char_before = this.textarea.value.substr( pos - 1, 1 );
+    if ( char_before == "\n" || char_before == "" ) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+};
+
+Textarea.Text.prototype.moveCaretAfter = function (text) {
+    var pos = this.textarea.value.lastIndexOf( text, this.caretPosition() );
+
+    if ( ! pos ) {
+        return;
+    }
+
+    this.moveCaret( ( pos - this.caretPosition() ) + 1 );
+};
