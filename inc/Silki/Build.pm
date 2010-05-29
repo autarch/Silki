ca868a96587817eaf2dbe96141f954c0f5d8b386
Dave Rolsky <autarch@urth.org>
2010-05-28 21:58:32 -0500

    DB install code works for fresh install, and is re-used for dev tool

--- inc/Silki/Build.pm
+++ inc/Silki/Build.pm
@@ -5,6 +5,44 @@ use warnings;
 
 use base 'Module::Build';
 
+sub new {
+    my $class = shift;
+    my %args  = @_;
 
+    $args{get_options} = {
+        'db-name'     => { type => '=s', default => 'Silki' },
+        'db-username' => { type => '=s' },
+        'db-password' => { type => '=s' },
+        'db-host'     => { type => '=s' },
+        'db-port'     => { type => '=s' },
+    };
+
+    return $class->SUPER::new(%args);
+}
+
+sub ACTION_install {
+    my $self = shift;
+
+    $self->SUPER::ACTION_install(@_);
+
+    $self->dispatch('database');
+}
+
+sub ACTION_database {
+    my $self = shift;
+
+    require Silki::DBInstaller;
+
+    my %db_config;
+
+    my %args = $self->args();
+
+    for my $key ( grep { defined $args{$_} } grep { /^db-/ } keys %args ) {
+        ( my $no_prefix = $key ) =~ s/^db-//;
+        $db_config{$no_prefix} = $args{$key};
+    }
+
+    Silki::DBInstaller->new(%db_config)->update_or_install_db();
+}
 
 1;
---
+++ inc/Silki/DBInstaller.pm
@@ -0,0 +1,300 @@
+package Silki::DBInstaller;
+
+use strict;
+use warnings;
+use autodie qw( :all );
+
+use lib 'lib';
+
+use Fey::DBIManager::Source;
+use File::Slurp qw( read_file);
+use File::Temp qw( tempdir);
+use Path::Class qw( file );
+
+use Moose;
+use MooseX::StrictConstructor;
+
+with 'MooseX::Getopt';
+
+has name => (
+    is       => 'rw',
+    writer   => '_set_name',
+    isa      => 'Str',
+    required => 1,
+);
+
+for my $attr (qw( username password host port )) {
+    has $attr => (
+        is     => 'rw',
+        writer => '_set_' . $attr,
+        isa    => 'Str',
+    );
+}
+
+has _existing_config => (
+    is      => 'ro',
+    isa     => 'HashRef[Str]',
+    lazy    => 1,
+    builder => '_build_existing_config',
+);
+
+has db_exists => (
+    is      => 'ro',
+    isa     => 'Bool',
+    lazy    => 1,
+    builder => '_build_db_exists',
+);
+
+has drop => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 0,
+);
+
+has seed => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 1,
+);
+
+sub BUILD {
+    my $self = shift;
+    my $p    = shift;
+
+    my $existing = $self->_existing_config();
+    unless ( exists $p->{name} ) {
+        die
+            "No name provided to the constructor and no name available from an existing Silki config file."
+            unless $existing->{name};
+
+        $self->_set_name( $p->{name} );
+    }
+
+    for my $attr (qw( username password host port )) {
+        my $set = '_set_' . $attr;
+
+        $self->$set( $existing->{$attr} )
+            if defined $existing->{$attr};
+    }
+
+    return;
+}
+
+sub run {
+    my $self = shift;
+
+    if ( !$self->drop() && $self->db_exists() ) {
+        warn
+            qq{\n  Will not drop a database unless you pass the --drop argument.\n\n};
+        exit 1;
+    }
+
+    print "\n";
+    $self->_drop_and_create_db();
+    $self->_build_db();
+
+    exit 0 unless $self->seed();
+
+    $self->_seed_data();
+}
+
+sub update_or_install_db {
+    my $self = shift;
+
+    my $version = $self->_get_installed_version();
+
+    print "\n";
+    my $name = $self->name();
+    _msg("Installing/updating your Silki database (database name = $name).");
+
+    if ( !defined $version ) {
+        _msg("Installing a fresh database.");
+        $self->_drop_and_create_db();
+        $self->_build_db();
+        $self->_seed_data();
+    }
+    else {
+        my $next_version = $self->_get_next_version();
+
+        if ( $version == $next_version ) {
+            _msg("Your Silki database is up-to-date.");
+            return;
+        }
+
+        _msg(
+            "Migrating your Silki database from version $version to $next_version."
+        );
+
+        $self->_migrate_db( $version, $next_version );
+    }
+}
+
+sub _build_db_exists {
+    my $self = shift;
+
+    return $self->_make_dbh() ? 1 : 0;
+}
+
+sub _get_installed_version {
+    my $self = shift;
+
+    my $dbh = $self->_make_dbh()
+        or return;
+
+    my $row = $dbh->selectrow_arrayref(q{SELECT version FROM "Version"});
+
+    return $row->[0] if $row;
+}
+
+sub _make_dbh {
+    my $self   = shift;
+
+    my $dsn = 'dbi:Pg:dbname=' . $self->name();
+
+    $dsn .= ';host=' . $self->host()
+        if defined $self->host();
+
+    $dsn .= ';port=' . $self->port()
+        if defined $self->port();
+
+    my %source = ( dsn => $dsn );
+
+    $source{username} = $self->username()
+        if defined $self->username();
+
+    $source{password} = $self->password()
+        if defined $self->password();
+
+    my $dbh = eval { Fey::DBIManager::Source->new(%source)->dbh() };
+
+    die $@ if $@ and $@ !~ /database "\w+" does not exist/;
+
+    return $dbh;
+}
+
+sub _build_existing_config {
+    my $self = shift;
+
+    my $config = eval {
+        require Silki::Config;
+
+        Silki::Config->instance()->_config_hash()->{db};
+    };
+
+    return $config || {};
+}
+
+sub _get_next_version {
+    my $self = shift;
+
+    my $file = file(qw( schema Silki.sql ));
+
+    my ($version_insert)
+        = grep {/INSERT INTO "Version"/}
+        read_file( $file->resolve()->stringify() );
+
+    my ($next_version) = $version_insert =~ /VALUES \((\d+)\)/;
+
+    die "Cannot find a version in the current schema!"
+        unless $next_version;
+
+    return $next_version;
+}
+
+sub _drop_and_create_db {
+    my $self = shift;
+
+    my $name = $self->name();
+
+    my $command = <<"EOF";
+SET CLIENT_MIN_MESSAGES = ERROR;
+
+DROP DATABASE IF EXISTS "$name";
+
+CREATE DATABASE "$name"
+       ENCODING 'UTF8';
+EOF
+
+    my $dir = tempdir( CLEANUP => 1 );
+    my $file = file( $dir, 'recreate-db.sql' );
+
+    open my $fh, '>', $file;
+    print {$fh} $command;
+    close $fh;
+
+    _msg(
+        "Dropping (if necessary) and creating the Silki database (database name = $name)"
+    );
+
+    system(
+        'psql', 'template1', $self->_psql_args(), '-q', '-f',
+        $file
+    );
+}
+
+sub _build_db {
+    my $self = shift;
+
+    my $schema_file = file( 'schema', 'Silki.sql' );
+
+    _msg("Creating schema from $schema_file");
+
+    system(
+        'psql', $self->name(), $self->_psql_args(), '-q', '-f',
+        $schema_file
+    ) and die 'Cannot create Silki database with psql';
+}
+
+sub _psql_args {
+    my $self = shift;
+
+    my @args;
+
+    if ( $self->username() ) {
+        push @args, '-U', $self->username();
+    }
+
+    if ( $self->password() ) {
+        push @args, '-W', $self->password();
+    }
+
+    if ( $self->host() ) {
+        push @args, '-h', $self->host();
+    }
+
+    if ( $self->port() ) {
+        push @args, '-p', $self->port();
+    }
+
+    return @args;
+}
+
+sub _seed_data {
+    my $self = shift;
+
+    my %config = ( name => $self->name() );
+
+    for my $key (qw( username password host port )) {
+        $config{$key} = $self->$key()
+            if defined $self->$key();
+    }
+
+    require Silki::Config;
+
+    local Silki::Config->_config_hash()->{db} = \%config;
+
+    require Silki::SeedData;
+
+    my $name = $self->name();
+    _msg("Seeding the $name database");
+
+    Silki::SeedData::seed_data('verbose');
+}
+
+sub _msg {
+    my $msg = shift;
+
+    print "  $msg\n\n";
+}
+
+1;
