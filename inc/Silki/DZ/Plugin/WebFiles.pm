5e88cc0cb46dabcc4efb2a483a1372c8e7e9af24
Dave Rolsky <autarch@urth.org>
2010-05-30 22:15:05 -0500

    make sure generated files match what webapp expects

--- inc/Silki/DZ/Plugin/WebFiles.pm
+++ inc/Silki/DZ/Plugin/WebFiles.pm
@@ -19,14 +19,14 @@ sub gather_files {
     my ( $self, $arg ) = @_;
 
     my $css = Dist::Zilla::File::InMemory->new(
-        name    => 'share/css/silki.css',
+        name    => 'share/css/silki-combined.css',
         content => Silki::Web::CSS->new()->create_content(),
     );
 
     $self->add_file($css);
 
     my $js = Dist::Zilla::File::InMemory->new(
-        name    => 'share/javascript/silki.js',
+        name    => 'share/js/silki-combined.js',
         content => Silki::Web::Javascript->new()->create_content(),
     );
 
