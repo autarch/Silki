6462de51d15d406681e16aebc41d21fb59e44351
Dave Rolsky <autarch@urth.org>
2010-09-12 10:46:08 -0500

    Move preview side-by-side with textarea
    Limit height of textarea and preview to fit on screen.

--- share/js-source/Silki/PageEdit.js
+++ share/js-source/Silki/PageEdit.js
@@ -5,12 +5,23 @@ if ( typeof Silki == "undefined" ) {
 }
 
 Silki.PageEdit = function () {
-    this.form = $("form-and-preview");
+    this.form    = $("edit-form");
+    this.preview = $("preview");
 
-    if ( ! this.form ) {
+    if ( ! ( this.form && this.preview ) ) {
         return;
     }
 
     this.toolbar = new Silki.PageEdit.Preview ();
     this.toolbar = new Silki.PageEdit.Toolbar ();
+
+    this._resizeFormAndPreview();
+};
+
+Silki.PageEdit.prototype._resizeFormAndPreview = function () {
+    /* This is a hacky guess */
+    var available = window.innerHeight * 0.7;
+
+    this.form.style.height    = available + "px";
+    this.preview.style.height = available + "px";
 };
